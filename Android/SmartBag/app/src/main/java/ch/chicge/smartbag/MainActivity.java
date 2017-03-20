dd0a21648b5e73fa332af3a94fe6ba02e9585f31
adrien.taboadac <adrien.taboada-cid@etu.hesge.ch>
2017-03-20 11:11:10 +0100

    Ajout lib a AndroidStudio

--- Android/SmartBag/app/src/main/java/ch/chicge/smartbag/MainActivity.java
+++ Android/SmartBag/app/src/main/java/ch/chicge/smartbag/MainActivity.java
@@ -3,11 +3,27 @@ package ch.chicge.smartbag;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.phychips.audio.*;
+import com.phychips.rcp.*;
+import com.phychips.utility.*;
 
+public abstract class MainActivity extends Activity implements iRcpEvent {
+	RcpApi aretePop;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		aretePop = new RcpApi();
+	}
+
+	void areteConnect() {
+		try {
+			aretePop.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }
