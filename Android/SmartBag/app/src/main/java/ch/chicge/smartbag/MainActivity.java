6e70dcd64e168f0c4450244a32558944a4ecfe9b
vincent.fontana <vincent.fontana@etu.hesge.ch>
2017-03-27 11:18:49 +0200

    ajout text and button from java

--- Android/SmartBag/app/src/main/java/ch/chicge/smartbag/MainActivity.java
+++ Android/SmartBag/app/src/main/java/ch/chicge/smartbag/MainActivity.java
@@ -1,19 +1,71 @@
 package ch.chicge.smartbag;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ch.chicge.smartbag.application.base;
+import ch.chicge.smartbag.application.scan;
 
 public class MainActivity extends Activity {
 
+
+    private LinearLayout layout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		base b = new base();
-        b.setBaseView();
+        layout = new LinearLayout(this);
+        scan s = new scan(layout, this);
+        setContentView(s.getLayout());
+
+
+		//layout = new LinearLayout(this);
+        //layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+        //LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        /*Button b = new Button(this);
+        b.setText("mon boutton");
+        //b.setLayoutParams(lparams);
+        Button b2 = new Button(this);
+        b2.setLeft(800);
+        b2.setTop(800);
+        b2.setText("mon deuxième boutton");
+        //b2.setLayoutParams(lparams);
+        layout.addView(b);
+        layout.addView(b2);*/
+
+        /*TextView alert = new TextView(this);
+        alert.setText("2 appareil inconnu, veulliez éloigner un appareil!");
+        alert.setTextColor(Color.red(20));
+        alert.setHeight(200);
+        alert.se
+        layout.addView(alert);*/
+
+
+
+
+        /*for (int i = 0; i < 2; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            for (int j = 0; j < 2; j++) {
+                Button btnTag = new Button(this);
+                btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                btnTag.setText("Button " + (j + 1 + (i * 4)));
+                btnTag.setId(j + 1 + (i * 4));
+                row.addView(btnTag);
+            }
+
+            layout.addView(row);
+        }*/
+
+        //
 	}
 
 
--- Android/SmartBag/app/src/main/java/ch/chicge/smartbag/application/scan.java
+++ Android/SmartBag/app/src/main/java/ch/chicge/smartbag/application/scan.java
@@ -1,10 +1,13 @@
 package ch.chicge.smartbag.application;
 
 import android.app.Activity;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import ch.chicge.smartbag.MainActivity;
 import ch.chicge.smartbag.interfacage.tag;
 import ch.chicge.smartbag.interfacage.utilitaire;
 
@@ -15,11 +18,26 @@ import ch.chicge.smartbag.interfacage.utilitaire;
 public class scan extends Activity {
 
     private ArrayList<tag> listTag = new ArrayList<tag>();
+    private LinearLayout layout;
+    private MainActivity t;
 
-    public scan(){
+    public scan(LinearLayout layout, MainActivity t){
+        //layout = new LinearLayout(this);
+        this.layout = layout;
+        this.t = t;
+        layout.setOrientation(LinearLayout.VERTICAL);  //Can also be done in xml by android:orientation="vertical"
+        addAlert();
+        addUnknownTag("535-4534-3214-5214-3132");
+
+        //listTag = utilitaire.getProxiTags();
+        //PrintListe();
+
         //setContentView(R.layout.liste);
-        listTag = utilitaire.getProxiTags();
-        PrintListe();
+        //setContentView(layout);
+    }
+
+    public LinearLayout getLayout(){
+        return layout;
     }
 
     public void PrintListe(){
@@ -34,5 +52,29 @@ public class scan extends Activity {
     public void addLayout(String CustomName){
         //LinearLayout placeHolder = (LinearLayout) findViewById(R.id.layout);
         //getLayoutInflater().inflate(R.layout.second_layout, placeHolder);
+    }
+
+    public void addAlert(){
+        LinearLayout row = new LinearLayout(t);
+        row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        TextView alert = new TextView(t);
+        alert.setText("2 appareil inconnu, veulliez éloigner un appareil!");
+        alert.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        row.addView(alert);
+        layout.addView(row);
+    }
+
+    public void addUnknownTag(String id){
+        LinearLayout row = new LinearLayout(t);
+        row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        Button b = new Button(t);
+
+        TextView alert = new TextView(t);
+        b.setText("enregistrer: " + id);
+        b.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        row.addView(b);
+        layout.addView(row);
     }
 }
