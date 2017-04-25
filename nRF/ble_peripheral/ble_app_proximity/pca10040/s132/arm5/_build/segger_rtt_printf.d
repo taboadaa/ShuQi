2a0aa4ed88b08d88d660660c37a19962a3041daa
Taboada Adrien <adrien.taboada@outlook.com>
2017-04-25 16:47:30 +0200

    Nouvelle structure du projet

---
+++ nRF/ble_peripheral/ble_app_proximity/pca10040/s132/arm5/_build/segger_rtt_printf.d
@@ -0,0 +1,1 @@
+.\_build\segger_rtt_printf.o: RTE\nRF_Libraries\nRF52832_xxAA\SEGGER_RTT_printf.c
---
+++ nRF/ble_peripheral/ble_app_proximity/pca10040/s132/arm5/_build/sensorsim.d
@@ -0,0 +1,4 @@
+.\_build\sensorsim.o: RTE\nRF_Libraries\nRF52832_xxAA\sensorsim.c
+.\_build\sensorsim.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Libraries\4.0.0-2.alpha\sensorsim\sensorsim.h
+.\_build\sensorsim.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\sensorsim.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
