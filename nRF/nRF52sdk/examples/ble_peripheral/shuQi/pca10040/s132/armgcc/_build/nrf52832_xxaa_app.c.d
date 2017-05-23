1dddc5e1dddb642288853c2b7bd9563a36ede16e
Taboada Adrien <adrien.taboada@outlook.com>
2017-05-23 15:54:53 +0200

    Maj app.c

--- nRF/nRF52sdk/examples/ble_peripheral/shuQi/pca10040/s132/armgcc/_build/nrf52832_xxaa_app.c.d
+++ nRF/nRF52sdk/examples/ble_peripheral/shuQi/pca10040/s132/armgcc/_build/nrf52832_xxaa_app.c.d
@@ -23,9 +23,11 @@ _build/nrf52832_xxaa_app.c.o: ../../../app.c ../../../app.h \
  /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_pthreadtypes.h \
  /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/types.h \
  /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stdio.h \
- /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/string.h \
- /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/string.h \
- ../../../../../../components/libraries/util/nordic_common.h \
+ /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h \
+ /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ /opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ ../../../../../../components/device/compiler_abstraction.h \
  ../../../../../../components/device/nrf.h \
  ../../../../../../components/device/nrf52.h \
  ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
@@ -38,82 +40,30 @@ _build/nrf52832_xxaa_app.c.o: ../../../app.c ../../../app.h \
  ../../../../../../components/device/nrf51_to_nrf52.h \
  ../../../../../../components/device/nrf52_name_change.h \
  ../../../../../../components/device/compiler_abstraction.h \
- ../../../../../../components/libraries/util/app_error.h \
- /opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ ../../../../../../components/libraries/log/nrf_log.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/log/src/nrf_log_internal.h \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/string.h \
+ /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/string.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
  ../../../../../../components/libraries/util/sdk_errors.h \
  ../../../../../../components/softdevice/s132/headers/nrf_error.h \
- ../config/sdk_config.h \
- ../../../../../../components/libraries/util/nordic_common.h \
- ../../../../../../components/libraries/util/app_error_weak.h \
- ../../../../../../components/softdevice/s132/headers/ble.h \
- ../../../../../../components/softdevice/s132/headers/ble_ranges.h \
- ../../../../../../components/softdevice/s132/headers/ble_types.h \
- ../../../../../../components/softdevice/s132/headers/ble_gap.h \
- ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
- ../../../../../../components/softdevice/s132/headers/ble_l2cap.h \
- ../../../../../../components/softdevice/s132/headers/ble_err.h \
- ../../../../../../components/softdevice/s132/headers/nrf_error.h \
- ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
- ../../../../../../components/softdevice/s132/headers/ble_gattc.h \
- ../../../../../../components/softdevice/s132/headers/ble_gatts.h \
- ../../../../../../components/softdevice/s132/headers/ble_hci.h \
- ../../../../../../components/ble/common/ble_srv_common.h \
- ../../../../../../components/softdevice/s132/headers/ble_types.h \
- ../../../../../../components/libraries/util/app_util.h \
- ../../../../../../components/device/compiler_abstraction.h \
- ../../../../../../components/softdevice/s132/headers/ble_gap.h \
- ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
- ../../../../../../components/ble/common/ble_advdata.h \
- ../../../../../../components/ble/ble_advertising/ble_advertising.h \
- ../../../../../../components/softdevice/s132/headers/ble_gattc.h \
- ../../../../../../components/ble/common/ble_conn_params.h \
- ../../../../../../components/ble/common/ble_srv_common.h \
- ../../../../../../components/boards/boards.h \
- ../../../../../../components/drivers_nrf/hal/nrf_gpio.h \
- ../../../../../../components/drivers_nrf/hal/nrf_peripherals.h \
- ../../../../../../components/device/nrf52832_peripherals.h \
- ../../../../../../components/libraries/util/nrf_assert.h \
- ../../../../../../components/libraries/util/app_error.h \
- /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h \
- /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h \
- ../../../../../../components/boards/pca10040.h \
- ../../../../../../components/softdevice/common/softdevice_handler/softdevice_handler.h \
- ../../../../../../components/softdevice/s132/headers/nrf_sdm.h \
- ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
- ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
- ../../../../../../components/softdevice/s132/headers/nrf_error_sdm.h \
- ../../../../../../components/softdevice/common/softdevice_handler/ble_stack_handler_types.h \
- ../../../../../../components/softdevice/common/softdevice_handler/ant_stack_handler_types.h \
- ../../../../../../components/softdevice/common/softdevice_handler/app_ram_base.h \
- ../../../../../../components/libraries/timer/app_timer.h \
- ../../../../../../components/ble/peer_manager/peer_manager.h \
- ../../../../../../components/libraries/util/sdk_common.h \
- ../../../../../../components/libraries/util/sdk_os.h \
  ../../../../../../components/libraries/util/app_util.h \
  ../../../../../../components/libraries/util/sdk_macros.h \
- ../../../../../../components/ble/peer_manager/peer_manager_types.h \
- ../../../../../../components/ble/common/ble_gatt_db.h \
+ ../../../../../../components/libraries/log/nrf_log_ctrl.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/libraries/log/src/nrf_log_ctrl_internal.h \
  ../../../../../../components/libraries/util/app_util_platform.h \
  ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
  ../../../../../../components/softdevice/s132/headers/nrf_nvic.h \
  ../../../../../../components/libraries/util/nrf_assert.h \
- ../../../../../../components/ble/peer_manager/peer_database.h \
- ../../../../../../components/ble/peer_manager/peer_manager_internal.h \
- ../../../../../../components/libraries/util/sdk_errors.h \
- ../../../../../../components/libraries/fds/fds.h \
- ../../../../../../components/libraries/fstorage/fstorage.h \
- ../../../../../../components/libraries/experimental_section_vars/section_vars.h \
- ../../../../../../components/ble/common/ble_conn_state.h \
- ../../../../../../components/libraries/util/sdk_mapped_flags.h \
- ../../../../../../components/libraries/bsp/bsp_btn_ble.h \
- ../../../../../../components/libraries/bsp/bsp.h \
- ../../../../../../components/libraries/button/app_button.h \
- ../../../service_if.h \
- ../../../../../../components/libraries/log/nrf_log.h \
- ../../../../../../components/libraries/log/src/nrf_log_internal.h \
- ../../../../../../components/libraries/log/nrf_log_ctrl.h \
- ../../../../../../components/libraries/log/src/nrf_log_ctrl_internal.h \
- ../../../../../../components/ble/nrf_ble_qwr/nrf_ble_qwr.h
+ ../../../../../../components/libraries/util/app_error.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
 
 ../../../app.h:
 
@@ -165,11 +115,15 @@ _build/nrf52832_xxaa_app.c.o: ../../../app.c ../../../app.h \
 
 /opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stdio.h:
 
-/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/string.h:
+/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h:
 
-/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/string.h:
+/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h:
 
-../../../../../../components/libraries/util/nordic_common.h:
+../../../../../../components/libraries/util/app_util.h:
+
+/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h:
+
+../../../../../../components/device/compiler_abstraction.h:
 
 ../../../../../../components/device/nrf.h:
 
@@ -195,154 +149,50 @@ _build/nrf52832_xxaa_app.c.o: ../../../app.c ../../../app.h \
 
 ../../../../../../components/device/compiler_abstraction.h:
 
-../../../../../../components/libraries/util/app_error.h:
+../../../../../../components/libraries/log/nrf_log.h:
 
-/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h:
+../config/sdk_config.h:
+
+../../../../../../components/libraries/log/src/nrf_log_internal.h:
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/string.h:
+
+/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/string.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
 
 ../../../../../../components/libraries/util/sdk_errors.h:
 
 ../../../../../../components/softdevice/s132/headers/nrf_error.h:
-
-../config/sdk_config.h:
-
-../../../../../../components/libraries/util/nordic_common.h:
-
-../../../../../../components/libraries/util/app_error_weak.h:
-
-../../../../../../components/softdevice/s132/headers/ble.h:
-
-../../../../../../components/softdevice/s132/headers/ble_ranges.h:
-
-../../../../../../components/softdevice/s132/headers/ble_types.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gap.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_svc.h:
-
-../../../../../../components/softdevice/s132/headers/ble_l2cap.h:
-
-../../../../../../components/softdevice/s132/headers/ble_err.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_error.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gatt.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gattc.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gatts.h:
-
-../../../../../../components/softdevice/s132/headers/ble_hci.h:
-
-../../../../../../components/ble/common/ble_srv_common.h:
-
-../../../../../../components/softdevice/s132/headers/ble_types.h:
-
-../../../../../../components/libraries/util/app_util.h:
-
-../../../../../../components/device/compiler_abstraction.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gap.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gatt.h:
-
-../../../../../../components/ble/common/ble_advdata.h:
-
-../../../../../../components/ble/ble_advertising/ble_advertising.h:
-
-../../../../../../components/softdevice/s132/headers/ble_gattc.h:
-
-../../../../../../components/ble/common/ble_conn_params.h:
-
-../../../../../../components/ble/common/ble_srv_common.h:
-
-../../../../../../components/boards/boards.h:
-
-../../../../../../components/drivers_nrf/hal/nrf_gpio.h:
-
-../../../../../../components/drivers_nrf/hal/nrf_peripherals.h:
-
-../../../../../../components/device/nrf52832_peripherals.h:
-
-../../../../../../components/libraries/util/nrf_assert.h:
-
-../../../../../../components/libraries/util/app_error.h:
-
-/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdlib.h:
-
-/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/stdlib.h:
-
-../../../../../../components/boards/pca10040.h:
-
-../../../../../../components/softdevice/common/softdevice_handler/softdevice_handler.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_sdm.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_soc.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_error_soc.h:
-
-../../../../../../components/softdevice/s132/headers/nrf_error_sdm.h:
-
-../../../../../../components/softdevice/common/softdevice_handler/ble_stack_handler_types.h:
-
-../../../../../../components/softdevice/common/softdevice_handler/ant_stack_handler_types.h:
-
-../../../../../../components/softdevice/common/softdevice_handler/app_ram_base.h:
-
-../../../../../../components/libraries/timer/app_timer.h:
-
-../../../../../../components/ble/peer_manager/peer_manager.h:
-
-../../../../../../components/libraries/util/sdk_common.h:
-
-../../../../../../components/libraries/util/sdk_os.h:
 
 ../../../../../../components/libraries/util/app_util.h:
 
 ../../../../../../components/libraries/util/sdk_macros.h:
 
-../../../../../../components/ble/peer_manager/peer_manager_types.h:
+../../../../../../components/libraries/log/nrf_log_ctrl.h:
 
-../../../../../../components/ble/common/ble_gatt_db.h:
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/libraries/log/src/nrf_log_ctrl_internal.h:
 
 ../../../../../../components/libraries/util/app_util_platform.h:
 
 ../../../../../../components/softdevice/s132/headers/nrf_soc.h:
 
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
 ../../../../../../components/softdevice/s132/headers/nrf_nvic.h:
 
 ../../../../../../components/libraries/util/nrf_assert.h:
 
-../../../../../../components/ble/peer_manager/peer_database.h:
+../../../../../../components/libraries/util/app_error.h:
 
-../../../../../../components/ble/peer_manager/peer_manager_internal.h:
-
-../../../../../../components/libraries/util/sdk_errors.h:
-
-../../../../../../components/libraries/fds/fds.h:
-
-../../../../../../components/libraries/fstorage/fstorage.h:
-
-../../../../../../components/libraries/experimental_section_vars/section_vars.h:
-
-../../../../../../components/ble/common/ble_conn_state.h:
-
-../../../../../../components/libraries/util/sdk_mapped_flags.h:
-
-../../../../../../components/libraries/bsp/bsp_btn_ble.h:
-
-../../../../../../components/libraries/bsp/bsp.h:
-
-../../../../../../components/libraries/button/app_button.h:
-
-../../../service_if.h:
-
-../../../../../../components/libraries/log/nrf_log.h:
-
-../../../../../../components/libraries/log/src/nrf_log_internal.h:
-
-../../../../../../components/libraries/log/nrf_log_ctrl.h:
-
-../../../../../../components/libraries/log/src/nrf_log_ctrl_internal.h:
-
-../../../../../../components/ble/nrf_ble_qwr/nrf_ble_qwr.h:
+../../../../../../components/libraries/util/app_error_weak.h:
