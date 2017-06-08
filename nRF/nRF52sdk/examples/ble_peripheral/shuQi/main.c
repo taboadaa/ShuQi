4b81db47bbfffd272280c7d75b8566114876482d
ColletAxel <colletaxel15@gmail.com>
2017-06-08 15:47:44 +0200

    sk6812 .c +  .h
    clean and modularize

--- nRF/nRF52sdk/examples/ble_peripheral/shuQi/main.c
+++ nRF/nRF52sdk/examples/ble_peripheral/shuQi/main.c
@@ -80,31 +80,10 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_ble_qwr.h"
 #include "app.h"
+#include "sk6812.h"
 
 
-/*******************************************************************************************************************
- * START SPI*******************************************************************************************************
- ******************************************************************************************************************/
 
-
-#include "nrf_drv_spi.h"
-#include "app_util_platform.h"
-#include "nrf_gpio.h"
-#include "nrf_delay.h"
-#define NRF_LOG_MODULE_NAME "APP"
-
-
-#define SPI_INSTANCE  0 /**< SPI instance index. */
-static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
-static volatile bool spi_xfer_done = true;  /**< Flag used to indicate that SPI instance completed the transfer. */
-
-static uint8_t       m_tx_buf[12+35];           /**< TX buffer. */
-static uint8_t       m_rx_buf[13];    /**< RX buffer. */
-static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. */
-
-/*******************************************************************************************************************
- * END SPI   *******************************************************************************************************
- ******************************************************************************************************************/
 
 #define IS_SRVC_CHANGED_CHARACT_PRESENT  0                                          /**< Include or not the service_changed characteristic. if not enabled, the server's database cannot be changed for the lifetime of the device*/
 
@@ -150,102 +129,7 @@ static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. *
 
 
 
-/*******************************************************************************************************************
- * START SPI*******************************************************************************************************
- ******************************************************************************************************************/
 
-/**
- * @brief SPI user event handler.
- * @param event
- */
-void spi_event_handler(nrf_drv_spi_evt_t const * p_event){
-    spi_xfer_done = true;
-}
-
-/**
- * @brief SPI user event handler.
- * @param uint8_t r,g,b : color
- * @param uint8_t* buffer ( buffer de 12 bytes )
- *
- *
-*/
-
-uint32_t data_led_rgb (uint8_t* color, uint8_t* buffer){
-   uint8_t i,j;
-   uint8_t tmp; // variable temporaire
-   uint8_t pos_buffer=0;
-
-   for ( j =0; j<3;j++){
-      for ( i =0; i<8;i+=2){
-         tmp = (color[j]>>i) & 0x03;
-
-         switch (tmp){
-          case 0 :
-             buffer[pos_buffer] = 0xCC;
-          break;
-          case 1 :
-             buffer[pos_buffer] = 0xCE;
-          break;
-
-          case 2:
-             buffer[pos_buffer] = 0xEC;
-          break;
-
-          case 3:
-             buffer[pos_buffer] = 0xEE;
-          break;
-
-          default :
-            return 1;
-         }
-         pos_buffer++;
-      }
-   }
-
-   return 0;
-
-}
-
-uint32_t sk6812_init (){
-	NRF_LOG_INFO("SPI example\r\n");
-
-	nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
-
-
-	spi_config.frequency = (0x33333300UL);
-	spi_config.mosi_pin = 4;
-	spi_config.sck_pin  = 3;
-	APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler));
-	return 0;
-
-}
-
-uint32_t sk6812_set_color( uint8_t r,uint8_t g,uint8_t b){
-
-
-	static uint8_t sk6812_init_state = 0;
-	if (!(sk6812_init_state)){
-		sk6812_init ();
-		sk6812_init_state = 1;
-	}
-	if (spi_xfer_done == false){
-		return 2;
-	}
-	spi_xfer_done = false;
-	uint8_t color[3] = {r,g,b};
-	if (data_led_rgb (color, m_tx_buf)){
-	   return 1;
-	}
-
-	for(uint8_t i = 12; i<m_length;i++){
-		m_tx_buf[i] = 0;
-	}
-	APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi, m_tx_buf, m_length, m_rx_buf, m_length));
-	return 0;
-}
-/*******************************************************************************************************************
- * END SPI   *******************************************************************************************************
- ******************************************************************************************************************/
 
 static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID; /**< Handle of the current connection. */
 static nrf_ble_qwr_t m_qwr; /**< Queued Writes structure.*/
@@ -805,7 +689,7 @@ int main_spi(void)
 	{
 		// Reset rx buffer and transfer done flag
 
-		sk6812_set_color(0x55,0x00,0xff);
+		sk6812_set_color( 0x00,0xFF,0x49);
 		while(spi_xfer_done == false)
 		{
 			__SEV();
---
+++ nRF/nRF52sdk/examples/ble_peripheral/shuQi/sk6812.h
@@ -0,0 +1,46 @@
+/*
+ * sk6812.h
+ *
+ *  Created on: 8 juin 2017
+ *      Author: colleta
+ */
+
+#ifndef SK6812_H_
+#define SK6812_H_
+
+#include "nrf_drv_spi.h"
+#include "app_util_platform.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#define NRF_LOG_MODULE_NAME "APP"
+
+
+#define SPI_INSTANCE  0 /**< SPI instance index. */
+static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
+static volatile bool spi_xfer_done = true;  /**< Flag used to indicate that SPI instance completed the transfer. */
+
+static uint8_t       m_tx_buf[12+35];           /**< TX buffer. */
+static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. */
+
+
+/**
+ * @brief SPI user event handler.
+ * @param event
+ */
+void spi_event_handler(nrf_drv_spi_evt_t const * p_event);
+
+/**
+ * @brief SPI user event handler.
+ * @param uint8_t r,g,b : color
+ * @param uint8_t* buffer ( buffer de 12 bytes )
+ *
+ *
+*/
+
+uint32_t data_led_rgb (uint8_t* color, uint8_t* buffer);
+
+uint32_t sk6812_init ();
+
+uint32_t sk6812_set_color( uint8_t r,uint8_t g,uint8_t b);
+
+#endif /* SK6812_H_ */
